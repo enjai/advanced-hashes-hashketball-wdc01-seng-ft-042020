cffce23820ef070d5f260edb2f0efe3c246e0c36
Injae Lee <ilee.injae@gmail.com>
2020-04-15 07:47:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,7 +135,7 @@ def num_points_scored(player_name)
   game_hash.each do |home_away, team_data| 
     #iterating over the first layer
     team_data[:players].each do |stats| 
-      #iterating over the second layer 
+      #iterating over the second 
      if stats[:player_name] == player_name 
         points = stats[:points]
       end 
